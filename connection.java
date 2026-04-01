75ffffd1e06b6de62a107cb5da9ba27d0b369234
abbyk06 <abbyskim.work@gmail.com>
2026-04-01 17:07:01 -0400

    add maven

---
+++ connection.java
@@ -0,0 +1,9 @@
+package E-Hotel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+Connection db = DriverManager.getConnection(url, username, password);
+
+
+
